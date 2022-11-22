0018cb53a1ed6ffc4042b8c72fb52659e247d7a3
Krischan Woehrer <60715629+kwoehrer@users.noreply.github.com>
2022-11-22 16:01:43 -0600

    Created branch.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+
+# Create your models here.
+
+# SPECIALITY_CHOICES = (
+#     ('Cardiologist', 'Cardiologist'),
+#     ('Pediatrician', 'Pediatrician'),
+#     ('Radiologist', 'Radiologist'),
+# )
+
+class Speciality(models.TextChoices):
+    Cardiologist = 'Cardiologist'
+    Pediatrician = 'Pediatrician'
+    Radiologist = 'Radiologist'
+
+
+class Physician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    speciality = models.CharField(max_length=50, choices=Speciality.choices)
+
+    def __str__(self):
+        return f"First Name: {self.first_name}, Last Name: {self.last_name}"
+
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    problem = models.TextField()
+    physician = models.ForeignKey(Physician, on_delete=models.CASCADE, null=True)
+
+
+class Appointment(models.Model):
+    physician = models.ForeignKey(Physician, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    appointment_time = models.DateTimeField()
+
+
+class Secretary(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+
+from django.db import models
+
 # Create your models here.

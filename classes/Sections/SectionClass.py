537282a6c8bde4f4097e659192b21538b3347fa3
luke hodory <lkhodory@gmail.com>
2022-11-28 19:18:31 -0600

    krischan helped with pr

--- classes/Sections/SectionClass.py
+++ classes/Sections/SectionClass.py
@@ -1,21 +1,25 @@
-class sectionClass():
-    def __init__(self, currentTA, sectionNumber, parentCourse, meetTime):
-        self.currentTA = currentTA
-        self.sectionNumber = sectionNumber
-        self.parentCourse = parentCourse
-        self.meetTime = meetTime
+from app.models import Section, TA, TASectionAssignments, Course
+from classes.Courses import Course
 
-    def getParentCourse(self):
-        return self.parentCourse
+class ConcreteSection():
+    def __init__(self, section:Section):
+        ta_pk = TASectionAssignments.Objects.get(section_ID=section.ID).account_ID
+        self.ta = TA.objects.get(account_ID=ta_pk)
+        self.parentCourse = Course.objects.get(course_ID=section.course_ID)
+        self.meetingTime = Section.MeetingTimes
+        self.sectionNumber = Section.section_num
+
+    def getParentCourse(self)->AbstractCourse:
+        return ConcreteCourse(self.parentCourse)
 
     def getSectionNumber(self):
         return self.sectionNumber
 
-    def getTA(self):
-        return self.currentTA
+    def getTA(self)->AbstractUser:
+        return TA_User(self.ta)
 
     def setTA(self, newTA):
-        self.currentTA = newTA
+        self.ta = newTA
 
     def getMeetTime(self):
         return self.meetTime

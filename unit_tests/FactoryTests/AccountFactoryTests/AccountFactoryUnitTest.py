074630f0d4febe13930df6f435f96df4b76cd529
Krischan Woehrer <60715629+kwoehrer@users.noreply.github.com>
2022-11-29 12:42:31 -0600

    AccountFactory createAccount unit tests done.

--- unit_tests/FactoryTests/AccountFactoryTests/AccountFactoryUnitTest.py
+++ unit_tests/FactoryTests/AccountFactoryTests/AccountFactoryUnitTest.py
@@ -106,7 +106,38 @@ class TestCreateAccount(TestCase):
 
 
 class TestDeleteCourse(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        User.objects.create(username='testadmin', password='password1', first_name="admin", last_name='admin',
+                            phone_number=1234567890, home_address='123 Hell Lane', user_type='Admin',
+                            email='adminemail@aol.com')
+        admin_user_model = Admin.objects.filter(username='testadmin')[0]
+        admin_model = Admin.objects.create(account_ID=admin_user_model.account_ID)
+
+        User.objects.create(username='ta', password='password1', first_name="ta", last_name='ta',
+                            phone_number=1234567890, home_address='123 Hell Lane', user_type='TA',
+                            email='taemail@aol.com')
+        ta_user_model = TA.objects.filter(username='ta')[0]
+        ta_model = TA.objects.create(account_ID=ta_user_model.account_ID)
+
+        User.objects.create(username='instructor', password='password1', first_name="instr", last_name='instr',
+                            phone_number=1234567890, home_address='123 Hell Lane', user_type='Instrutor',
+                            email='instremail@aol.com')
+        instr_user_model = Instructor.objects.filter(username='instructor')[0]
+        instr_model = Instructor.objects.create(account_ID=instr_user_model.account_ID)
+
+        self.acc_fact: AbstractAccountFactory = ConcreteAccountFactory()
+        self.good_account_attributes = dict()
+        self.good_account_attributes['username'] = 'username'
+        self.good_account_attributes['password'] = 'password1'
+        self.good_account_attributes['first_name'] = 'John'
+        self.good_account_attributes['last_name'] = 'Doe'
+        self.good_account_attributes['phone_number'] = '2622622662'
+        self.good_account_attributes['home_address'] = '123 Mary Jane Lane'
+        self.good_account_attributes['email'] = 'newemail@aol.com'
+        self.good_account_attributes['user_type'] = 'TA'
+        self.admin: User = Admin_User(admin_model)
+        self.ta: User = Ta_User(ta_model)
+        self.instr: User = Instructor_User(instr_model)
 
 
 class TestEditCourse(unittest.TestCase):

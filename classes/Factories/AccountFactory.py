7a32055248cc5990bfbca0a92632340aa065e1b1
Krischan Woehrer <60715629+kwoehrer@users.noreply.github.com>
2022-11-29 15:27:23 -0600

    Bug fix.

--- classes/Factories/AccountFactory.py
+++ classes/Factories/AccountFactory.py
@@ -13,8 +13,43 @@ class AbstractAccountFactory(ABC):
 
 class ConcreteAccountFactory(AbstractAccountFactory):
 
-    def create_account(self, creator: User, newAccountAttrbitutes: []):
-        pass
+    def create_account(self, creator: User, newAccountAttributes: []):
+        # Verify creator is an admin
+        if not (isinstance(creator, Admin_User)):
+            raise TypeError("Only admin user accounts can create accounts.")
+        # Verify newAccountAttributes is correct
+        username = newAccountAttributes['username']
+        password = newAccountAttributes['password']
+        if password == "" or len(password) < 8:
+            raise ValueError("Password must be more than 8 characters long.")
+        first_name = newAccountAttributes['first_name']
+        last_name = newAccountAttributes['last_name']
+        phone_number = newAccountAttributes['phone_number']
+        home_address = newAccountAttributes['home_address']
+        email = newAccountAttributes['email']
+        user_type = newAccountAttributes['user_type']
 
-    def delete_account(self, deletor: User, newAccountAttrbitutes: []):
-        pass
+        new_user = User(username=username, password=password, first_name=first_name, last_name=last_name,
+                        phone_number=phone_number, home_address=home_address, email=email, user_type=user_type)
+        new_user.save()
+
+        # look at type of user and create a reference to the user in its sub-table
+        new_subtype = None
+        if user_type == "Admin":
+            new_subtype = Admin(new_user.account_ID)
+        elif user_type == "TA":
+            new_subtype = TA(new_user.account_ID)
+        elif user_type == "Instructor":
+            new_subtype = Instructor(new_user.account_ID)
+
+        new_subtype.save()  # Should throw erorr if usertype was not correct.
+
+    def delete_account(self, deletor: User, deletee: User):
+        #verify our logged in user/deletor can delete accounts
+        if not (isinstance(deletor, Admin_User)):
+            raise TypeError("Only admin user accounts can delete accounts.")
+
+        deletee_id = deletee.get_id() #TODO MIGHT NEED TO FIX
+
+        User.objects.filter(account_ID=deletee_id).delete() #should propagate to subtable due to cascade effects
+

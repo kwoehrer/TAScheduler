a24ecfed5a0564c22e7fdd07f44f1bdea27e68c6
luke hodory <lkhodory@gmail.com>
2022-11-29 15:21:17 -0600

    started setup method

--- unit_tests/section_tests.py
+++ unit_tests/section_tests.py
@@ -1,22 +1,36 @@
 from django.test import TestCase
-from app.models import Section
+from app.models import Section, Course
+
 
 class SectionTest(TestCase):
     pass
 
-class test_get_parent(TestCase):
-    def setUp(self):
-        Section.objects.create(course_ID=, section_num=456, MeetingTimes='12:00', ta_account_id=)
 
-class test_get_section_num(TestCase):
+class TestGetParent(TestCase):
+    def setUp(self) -> None:
+        Course.objects.create(course_ID=101, name='Intro to Nonsense', semester='Spring', year=2022)
+        Section.objects.filter(course_ID=101)
+
+
+class TestGetSectionNum(TestCase):
     pass
-class test_set_section_num(TestCase):
+
+
+class TestSetSectionNum(TestCase):
     pass
+
+
 class test_get_TA(TestCase):
     pass
+
+
 class test_set_TA(TestCase):
     pass
+
+
 class test_get_meet_time(TestCase):
     pass
+
+
 class test_set_meet_time(TestCase):
     pass

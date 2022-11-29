65657a6e7f741cc53cb65d3c410d0ee9cb1a921b
KWoehrer <kriwoehrer@gmail.com>
2022-11-28 19:39:12 -0600

    Working on PR w/ team.

--- classes/Sections/SectionClass.py
+++ classes/Sections/SectionClass.py
@@ -1,25 +1,56 @@
 from app.models import Section, TA, TASectionAssignments, Course
 from classes.Courses import Course
+import abc
 
-class ConcreteSection():
+class AbstractSection(abc):
+    @abc.abstractmethod
+    def getParentCourse(self):
+        pass
+
+    @abc.abstractmethod
+    def getSectionNumber(self):
+        pass
+    @abc.abstractmethod
+    def getSectionNumber(self):
+        pass
+
+    @abc.abstractmethod
+    def getTA(self)->AbstractUser:
+        pass
+
+    @abc.abstractmethod
+    def setTA(self, newTA):
+        pass
+
+    @abc.abstractmethod
+    def getMeetTime(self):
+        pass
+
+    @abc.abstractmethod
+    def setMeetTime(self, newTime):
+        pass
+class ConcreteSection(AbstractSection):
     def __init__(self, section:Section):
-        ta_pk = TASectionAssignments.Objects.get(section_ID=section.ID).account_ID
-        self.ta = TA.objects.get(account_ID=ta_pk)
-        self.parentCourse = Course.objects.get(course_ID=section.course_ID)
-        self.meetingTime = Section.MeetingTimes
-        self.sectionNumber = Section.section_num
+        self.section = section
 
     def getParentCourse(self)->AbstractCourse:
-        return ConcreteCourse(self.parentCourse)
+        return ConcreteCourse(Course.objects.get(course_ID=self.section.course_ID))
 
     def getSectionNumber(self):
-        return self.sectionNumber
+        return self.section.section_num
 
     def getTA(self)->AbstractUser:
-        return TA_User(self.ta)
+        ta_pk = TASectionAssignments.Objects.get(section_ID= self.section.ID).account_ID
+        ta = TA.objects.get(account_ID=ta_pk)
+        return TA_User(ta)
 
-    def setTA(self, newTA):
-        self.ta = newTA
+    def setTA(self, newTA:AbstractUser):
+        if isinstance(newTA, TA_User):
+            ta_id = newTA.getID()
+
+        else:
+            raise TypeError
+
 
     def getMeetTime(self):
         return self.meetTime

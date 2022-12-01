3c02cec21525e1f8a36374a544deba6b2c415285
yhlin55 <lin55@uwm.edu>
2022-11-30 19:37:01 -0600

    Working on unit tests for course classes, finished up test cases for section methods and fixed some problems

--- unit_tests/CourseTest/CourseUnitTest.py
+++ unit_tests/CourseTest/CourseUnitTest.py
@@ -7,35 +7,29 @@ from classes.Sections.SectionClass import AbstractSection, ConcreteSection
 class TestAddInstructor(TestCase):
 
     def setUp(self) -> None:
-        self.course: AbstractCourse = ConcreteCourse()
 
-        Course.objects.create(name="course1", semester="Spring", year="2022",
+        course_model = Course.objects.create(name="course1", semester="Spring", year="2022",
                               description="first course", credits="3")
-        f_course_model = Course.objects.filter(name='course1')[0]
-        self.first_course_model = Course.objects.create(course_ID=f_course_model.course_ID)
+        self.course: AbstractCourse = ConcreteCourse(course_model)
 
-        Course.objects.create(name="course2", semester="Spring", year="2022",
+        s_course_model = Course.objects.create(name="course2", semester="Spring", year="2022",
                               description="second course", credits="3")
-        s_course_model = Course.objects.filter(name='course2')[0]
-        self.second_course_model = Course.objects.create(course_ID=s_course_model.course_ID)
+        self.course2: AbstractCourse = ConcreteCourse(s_course_model)
 
-        User.objects.create(email="Inst1@gmail.com", username="Inst1", password="inst1",
+        inst_model = User.objects.create(email="Inst1@gmail.com", username="Inst1", password="inst1",
                             first_name="first", last_name="instructor", phone_number="123", home_address="address",
                             user_type="Instructor")
-        instr_user_model = User.objects.filter(username='Inst1')[0]
-        self.instr_model = User.objects.create(account_ID=instr_user_model.account_ID)
+        self.inst: AbstractUser = ConcreteUser(inst_model)
 
-        User.objects.create(email="Ta1@gmail.com", username="Ta1", password="ta1",
+        ta_model = User.objects.create(email="Ta1@gmail.com", username="Ta1", password="ta1",
                             first_name="first", last_name="ta", phone_number="123", home_address="address",
                             user_type="TA")
-        ta_user_model = User.objects.filter(username='Ta1')[0]
-        self.ta_model = User.objects.create(account_ID=ta_user_model.account_ID)
+        self.ta:AbstractUser = ConcreteUser(ta_model)
 
-        User.objects.create(email="Ta2@gmail.com", username="Ta2", password="ta2",
+        ta2_model = User.objects.create(email="Ta2@gmail.com", username="Ta2", password="ta2",
                             first_name="first", last_name="ta", phone_number="123", home_address="address",
                             user_type="TA")
-        s_ta_user_model = User.objects.filter(username='Ta2')[0]
-        self.second_ta_model = User.objects.create(account_ID=s_ta_user_model.account_ID)
+        self.ta2 :AbstractUser = ConcreteUser(ta2_model)
 
     def add_Instr_no_Arg(self):
         with self.assertRaises(TypeError, msg="More than zero argument is required"):
@@ -43,44 +37,38 @@ class TestAddInstructor(TestCase):
 
     def add_Instr_more_arg(self):
         with self.assertRaises(TypeError, msg="add instr - Too much arg was added"):
-            self.course.add_instructor(self.instr_model, self.ta_model)
+            self.course.add_instructor(self.inst, self.ta)
 
     def add_Instr_wrong_User(self):
         with self.assertRaises(TypeError, msg="This user is not a instructor"):
-            self.course.add_instructor(self.ta_model)
+            self.course.add_instructor(self.ta)
 
 class TestAddTa(TestCase):
 
     def setUp(self) -> None:
-        self.course: AbstractCourse = ConcreteCourse()
 
-        Course.objects.create(name="course1", semester="Spring", year="2022",
+        f_course_model = Course.objects.create(name="course1", semester="Spring", year="2022",
                               description="first course", credits="3")
-        f_course_model = Course.objects.filter(name='course1')[0]
-        self.first_course_model = Course.objects.create(course_ID=f_course_model.course_ID)
+        self.course: AbstractCourse = ConcreteCourse(f_course_model)
 
-        Course.objects.create(name="course2", semester="Spring", year="2022",
+        s_course_model = Course.objects.create(name="course2", semester="Spring", year="2022",
                               description="second course", credits="3")
-        s_course_model = Course.objects.filter(name='course2')[0]
-        self.second_course_model = Course.objects.create(course_ID=s_course_model.course_ID)
+        self.course: AbstractCourse = ConcreteCourse(s_course_model)
 
-        User.objects.create(email="Inst1@gmail.com", username="Inst1", password="inst1",
+        inst_model = User.objects.create(email="Inst1@gmail.com", username="Inst1", password="inst1",
                             first_name="first", last_name="instructor", phone_number="123", home_address="address",
                             user_type="Instructor")
-        instr_user_model = User.objects.filter(username='Inst1')[0]
-        self.instr_model = User.objects.create(account_ID=instr_user_model.account_ID)
+        self.inst: AbstractUser = ConcreteUser(inst_model)
 
-        User.objects.create(email="Ta1@gmail.com", username="Ta1", password="ta1",
+        ta_model = User.objects.create(email="Ta1@gmail.com", username="Ta1", password="ta1",
                             first_name="first", last_name="ta", phone_number="123", home_address="address",
                             user_type="TA")
-        ta_user_model = User.objects.filter(username='Ta1')[0]
-        self.ta_model = User.objects.create(account_ID=ta_user_model.account_ID)
+        self.ta: AbstractUser = ConcreteUser(ta_model)
 
-        User.objects.create(email="Ta2@gmail.com", username="Ta2", password="ta2",
+        ta2_model = User.objects.create(email="Ta2@gmail.com", username="Ta2", password="ta2",
                             first_name="first", last_name="ta", phone_number="123", home_address="address",
                             user_type="TA")
-        s_ta_user_model = User.objects.filter(username='Ta2')[0]
-        self.second_ta_model = User.objects.create(account_ID=s_ta_user_model.account_ID)
+        self.ta2: AbstractUser = ConcreteUser(ta2_model)
 
     def add_ta_no_arg(self):
         with self.assertRaises(TypeError, msg="Add ta - more than one arg is required"):
@@ -88,33 +76,29 @@ class TestAddTa(TestCase):
 
     def add_ta_more_arg(self):
         with self.assertRaises(TypeError, msg="Add ta - Too much arg was added"):
-            self.course.add_ta(self.ta_model, self.second_ta_model)
+            self.course.add_ta(self.ta, self.ta2)
 
     def add_ta_wrong_user(self):
         with self.assertRaises(TypeError, msg="Add ta - this user is not a TA"):
-            self.course.add_ta(self.instr_model)
+            self.course.add_ta(self.inst)
 
 class TestremoveTA(TestCase):
 
     def setUp(self) -> None:
-        self.course: AbstractCourse = ConcreteCourse()
 
-        Course.objects.create(name="course1", semester="Spring", year="2022",
+        course_model = Course.objects.create(name="course1", semester="Spring", year="2022",
                               description="first course", credits="3")
-        f_course_model = Course.objects.filter(name='course1')[0]
-        self.first_course_model = Course.objects.create(course_ID=f_course_model.course_ID)
+        self.course: AbstractCourse = ConcreteCourse(course_model)
 
-        User.objects.create(email="Ta1@gmail.com", username="Ta1", password="ta1",
+        ta_model = User.objects.create(email="Ta1@gmail.com", username="Ta1", password="ta1",
                             first_name="first", last_name="ta", phone_number="123", home_address="address",
                             user_type="TA")
-        ta_user_model = User.objects.filter(username='Ta1')[0]
-        self.ta_model = User.objects.create(account_ID=ta_user_model.account_ID)
+        self.ta: AbstractUser = ConcreteUser(ta_model)
 
-        User.objects.create(email="Ta2@gmail.com", username="Ta2", password="ta2",
+        s_ta_model = User.objects.create(email="Ta2@gmail.com", username="Ta2", password="ta2",
                             first_name="first", last_name="ta", phone_number="123", home_address="address",
                             user_type="TA")
-        s_ta_user_model = User.objects.filter(username='Ta2')[0]
-        self.second_ta_model = User.objects.create(account_ID=s_ta_user_model.account_ID)
+        self.ta2: AbstractUser = ConcreteUser(s_ta_model)
 
     def remove_ta_no_arg(self):
         with self.assertRaises(TypeError, msg="Remove ta - No arg was added"):
@@ -122,12 +106,12 @@ class TestremoveTA(TestCase):
 
     def remove_ta_more_arg(self):
         with self.assertRaises(TypeError, msg="Remove ta - Too much arg was added"):
-            self.course.remove_ta(self.ta_model, self.second_ta_model)
+            self.course.remove_ta(self.ta, self.ta2)
 
     def remove_ta_no_user(self):
-        self.course.add_ta(self.ta_model)
+        self.course.add_ta(self.ta)
         with self.assertRaises(TypeError, msg="Remove ta - no such TA was found"):
-            self.course.remove_ta(self.second_ta_model)
+            self.course.remove_ta(self.ta2)
 
 
 class TestaddSection(TestCase):
@@ -135,12 +119,11 @@ class TestaddSection(TestCase):
     def setUp(self) -> None:
         course_model = Course.objects.create(name="course1", semester="Spring", year="2022", description="test",
                                              credits="3")
+        self.course: AbstractCourse = ConcreteCourse(course_model)
 
         section_model = Section.objects.create(course_model.course_ID, section_num=100, MeetingTime="9:00")
-
-
-        self.course: AbstractCourse = ConcreteCourse(course_model)
         self.section: AbstractSection = ConcreteSection(section_model)
+
     def add_section_no_arg(self):
         with self.assertRaises(TypeError, msg="add_section - More than zero arg is required"):
             self.course.add_section()

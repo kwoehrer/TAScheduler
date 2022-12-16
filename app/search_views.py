27187b7a5cb07fa64612f5e482ca2dc79808f09c
KWoehrer <kriwoehrer@gmail.com>
2022-12-16 17:43:12 -0600

    Starting work on user search results page.

--- app/search_views.py
+++ app/search_views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
-from app.models import User, Admin, Instructor, TA, Course, Section
-from classes.Courses.CoursesClass import ConcreteCourse, AbstractCourse
-from classes.Factories.AccountFactory import AbstractAccountFactory, ConcreteAccountFactory
-from classes.Factories.CourseFactory import ConcreteCourseFactory, AbstractCourseFactory
-from classes.Sections.SectionClass import ConcreteSection
-from classes.Users.users import TAUser, AbstractUser, AdminUser, InstructorUser
+from app.models import User
 
 
 class SearchHome(View):
     def get(self, request):
         t = None
         user_type = User.objects.get(account_ID=request.session['current_user_account_id']).user_type
-        if user_type == "TA" or  user_type == "Instructor" or user_type == "Admin":
+        if user_type == "TA" or user_type == "Instructor" or user_type == "Admin":
             t = "poopy"
 
         if t == None:
@@ -24,7 +19,7 @@ class SearchHome(View):
     def post(self, request):
         t = None
         user_type = User.objects.get(account_ID=request.session['current_user_account_id']).user_type
-        if user_type == "TA" or  user_type == "Instructor" or user_type == "Admin":
+        if user_type == "TA" or user_type == "Instructor" or user_type == "Admin":
             t = "hehexd"
 
         if t == None:
@@ -32,27 +27,54 @@ class SearchHome(View):
         else:
             return render(request, "searchStates/SearchHome.html", {})
 
+
 class SearchUser(View):
     def get(self, request):
         t = None
         user_type = User.objects.get(account_ID=request.session['current_user_account_id']).user_type
-        if user_type == "TA" or  user_type == "Instructor" or user_type == "Admin":
+        if user_type == "TA" or user_type == "Instructor" or user_type == "Admin":
             t = "hehexd"
 
         if t == None:
             return render(request, "login.html", {'message': "Please login to access search page."})
         else:
-            return render(request, "searchStates/UserSearch.html", {})
+            return render(request, "searchStates/UserSearch.html", {'page_state_title': "User Search Form"})
+
     def post(self, request):
         t = None
         user_type = User.objects.get(account_ID=request.session['current_user_account_id']).user_type
-        if user_type == "TA" or  user_type == "Instructor" or user_type == "Admin":
+        if user_type == "TA" or user_type == "Instructor" or user_type == "Admin":
             t = "hehexd"
 
         if t == None:
             return render(request, "login.html", {'message': "Please login to access search page."})
         else:
-            return render(request, "searchStates/UserSearch.html", {})
+            return render(request, "searchStates/UserSearch.html", {'page_state_title': "User Search Form"})
+
+class SearchUserResult(View):
+    def get(self, request):
+        t = None
+        user_type = User.objects.get(account_ID=request.session['current_user_account_id']).user_type
+        if user_type == "TA" or user_type == "Instructor" or user_type == "Admin":
+            t = "hehexd"
+
+        if t == None:
+            return render(request, "login.html", {'message': "Please login to access search page."})
+        else:
+            return render(request, "searchStates/UserSearchResults.html", {'page_state_title': "User Search Results"})
+
+    def post(self, request):
+        t = None
+        user_type = User.objects.get(account_ID=request.session['current_user_account_id']).user_type
+        if user_type == "TA" or user_type == "Instructor" or user_type == "Admin":
+            t = "hehexd"
+
+        if t == None:
+            return render(request, "login.html", {'message': "Please login to access search page."})
+        else:
+            return render(request, "searchStates/UserSearchResults.html", {'page_state_title': "User Search Results"})
+
+
 
 class SearchCourse(View):
     pass

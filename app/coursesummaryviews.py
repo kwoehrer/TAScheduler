c63d467b486d3d44701ba05dbeb00fec0e49d8ee
luke hodory <lkhodory@gmail.com>
2022-12-18 18:30:20 -0600

    added a few things to admin course summary template

--- app/coursesummaryviews.py
+++ app/coursesummaryviews.py
@@ -12,7 +12,25 @@ from classes.Users.users import TAUser, AbstractUser, AdminUser, InstructorUser
 
 class CourseSummary(View):
     def get(self, request):
-        pass
+        course_model = Course.objects.get(account_ID=request.GET.get(['current_course']))
+        course_wrapper = Course.ConcreteCourse(course_model)
+
+        t = None
+        user_type = User.objects.get(account_ID=request.session['current_user_account_id']).user_type
+        if user_type == "TA":
+            t = render(request, "./CourseSummaryStates/TACourseSummary.html",
+                       {'course_wrapper': course_wrapper})
+        elif user_type == "Instructor":
+            t = render(request, "./CourseSummaryStates/InstructorCourseSummary.html",
+                       {'course_wrapper': course_wrapper})
+        elif user_type == "Admin":
+            t = render(request, "./CourseSummaryStates/AdminCourseSummary.html",
+                       {'course_wrapper': course_wrapper})
+
+        if t == None:
+            return render(request, "login.html", {'message': "An unknown error has occurred."})
+        else:
+            return render(request, "CourseSummary.html", {'State': t})
 
     def post(self, request):
         course_model = Course.objects.get(account_ID=request.POST.get(['current_course']))

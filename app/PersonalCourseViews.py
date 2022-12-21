79c49bb45335f85e4027e6a6a3336332d19a5bb6
KWoehrer <kriwoehrer@gmail.com>
2022-12-20 23:57:47 -0600

    Personal CoursePage done for TA.

--- app/PersonalCourseViews.py
+++ app/PersonalCourseViews.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 
-from app.models import User, Admin, Instructor, TA, Course
+from app.models import User, Instructor, TA
 from classes.Courses.CoursesClass import ConcreteCourse
-from classes.Users.users import InstructorUser, TAUser, AdminUser
+from classes.Users.users import InstructorUser, TAUser
 
 
 class MyCourses(View):
@@ -15,17 +15,33 @@ class MyCourses(View):
         if user_type == "TA":
             curr_user_model = TA.objects.get(account_ID=curr_user_model)
             logged_user = TAUser(curr_user_model)
+
+            concrete_course_list = list()
+            course_list = logged_user.getCourses()
+            section_dict = dict()
+
+            for course in course_list:
+                course = ConcreteCourse(course)
+                for section in course.get_sections():
+                    if section.getTA().getID() == logged_user.getID():
+                        if course.get_course_id() not in section_dict:
+                            section_dict[course.get_course_id()] = list()
+                        section_dict[course.get_course_id()].append(section.getSectionNumber())
+                concrete_course_list.append(course)
+
+            print(section_dict)
+            return render(request, "MyCourseAssignments.html",
+                          {"my_courses": concrete_course_list, "my_sections": section_dict })
         elif user_type == "Instructor":
             curr_user_model = Instructor.objects.get(account_ID=curr_user_model)
             logged_user = InstructorUser(curr_user_model)
-        elif user_type == "Admin":
-            curr_user_model = Admin.objects.get(account_ID=curr_user_model)
-            logged_user = AdminUser(curr_user_model)
+            course_list = logged_user.getCourses()
+            return render(request, "MyCourseAssignments.html", {"my_courses": course_list})
 
         if logged_user == None:
-            return render(request, "login.html", {'message': "Please login to access  your personal course assignments."})
+            return render(request, "login.html",
+                          {'message': "Please login to access  your personal course assignments."})
 
-        #Create course array
         course_list = logged_user.getCourses()
 
         return render(request, "MyCourseAssignments.html", {"my_courses": course_list})
